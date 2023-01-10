17eaaf760c1fdc954b13a1614e34dacd697d9b6a
Astroth1984 <elmounir.othmane666@gmail.com>
2023-01-10 21:51:15 +0100

    spring boot dev tools

---
+++ 02-spring-boot-dev-tools/src/main/java/com/docxdoc/springboot/demo/starterapp/rest/LandingPageRestController.java
@@ -0,0 +1,22 @@
+package com.docxdoc.springboot.demo.starterapp.rest;
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LandingPageRestController {
+
+	@GetMapping("/")
+	public String landingPageController() {
+		return "Hello World. Time on Server is : " + LocalDateTime.now();
+	}
+	
+	@GetMapping("/workout")
+	public String getDailyWorkout() {
+		return "Run 100km";
+	}
+	
+	
+}
